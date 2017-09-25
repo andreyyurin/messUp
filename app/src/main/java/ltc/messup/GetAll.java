23b02a98373657b45a81cdc1bf4ceccda76de00c
fastvepik <andrey.yurin55@gmail.com>
2017-09-25 23:19:17 +0300

    added Fab, names, imgs; fixed bug with button

--- app/src/main/java/ltc/messup/GetAll.java
+++ app/src/main/java/ltc/messup/GetAll.java
@@ -2,46 +2,141 @@ package ltc.messup;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dd.processbutton.iml.ActionProcessButton;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+import com.squareup.picasso.Picasso;
+import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.api.VKApi;
 import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
 import com.vk.sdk.api.VKParameters;
 import com.vk.sdk.api.VKRequest;
 import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKApiUser;
+import com.vk.sdk.api.model.VKApiUserFull;
+import com.vk.sdk.api.model.VKList;
 
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by admin on 16.08.2017.
  */
 
-public class GetAll extends AppCompatActivity {
+public class GetAll extends AppCompatActivity implements RewardedVideoAdListener {
 
     private ActionProcessButton btnGo;
     private DiscreteSeekBar seekBar;
     private int value;
+    private Toolbar toolbar;
+    private CircleImageView profileImg;
+    private FloatingActionButton fab;
     private TextView textSeek;
+    private AdView mAdView;
+    private static final String APP_ID = "ca-app-pub-8154277548860310~8692901258";
+    private RewardedVideoAd mAd;
+    private TextView textName;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.getall_main);
 
+        MobileAds.initialize(getApplicationContext(), APP_ID);
+
+        textName = (TextView) findViewById(R.id.textName);
         seekBar = (DiscreteSeekBar) findViewById(R.id.seekbar);
         btnGo = (ActionProcessButton) findViewById(R.id.btnGo);
         btnGo.setMode(ActionProcessButton.Mode.ENDLESS);
-
-        value = seekBar.getProgress();
+        profileImg = (CircleImageView) findViewById(R.id.profile_image);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         textSeek = (TextView) findViewById(R.id.textSeek);
+
+        fab.setBackgroundColor(getResources().getColor(R.color.toolbar_color));
+
+        mAd = MobileAds.getRewardedVideoAdInstance(this);
+        //loadRewardedVideoAd();
+
+        setSupportActionBar(toolbar);
+        seekBar.setProgress(1);
+        value = seekBar.getProgress();
+
+        textSeek.setText(String.valueOf(value));
+
+
+        VKRequest request = VKApi.users().get(VKParameters.from(VKApiConst.FIELDS, "photo_max_orig"));
+        request.executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                VKApiUser user = ((VKList<VKApiUser>) response.parsedModel).get(0);
+                String urlImage = user.photo_max_orig;
+                Log.d("xyi", urlImage);
+                Picasso
+                        .with(getApplicationContext())
+                        .load(urlImage)
+                        .into(profileImg);
+            }
+            @Override
+            public void onError(VKError error) {}
+            @Override
+            public void attemptFailed(VKRequest request, int attemptNumber, int totalAttempts) {}
+        });
+
+
+        request = new VKRequest("account.getProfileInfo");
+
+        request.executeWithListener(new VKRequest.VKRequestListener()
+        {
+            @Override
+            public void onComplete(VKResponse response) {
+                super.onComplete(response);
+
+                String status = "";
+
+                try {
+
+                    JSONObject jsonObject = response.json.getJSONObject("response");
+
+                    String first_name = jsonObject.getString("first_name");
+                    String last_name = jsonObject.getString("last_name");
+                    String screen_name = jsonObject.getString("screen_name");
+                    status = jsonObject.getString("status");
+                    textName.setText(first_name+" "+last_name);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadRewardedVideoAd();
+            }
+        });
+
         seekBar.setOnProgressChangeListener(new DiscreteSeekBar.OnProgressChangeListener() {
             @Override
             public void onProgressChanged(DiscreteSeekBar seekBar, int progress, boolean fromUser) {
@@ -89,6 +184,12 @@ public class GetAll extends AppCompatActivity {
                                         Log.d("ANSWER", "УСПЕХ");
                                         btnGo.setProgress(0);
                                     }
+
+                                    @Override
+                                    public void onProgress(VKRequest.VKProgressType progressType, long bytesLoaded, long bytesTotal) {
+                                        super.onProgress(progressType, bytesLoaded, bytesTotal);
+                                        btnGo.setProgress(btnGo.getProgress()+1);
+                                    }
                                 });
                                 try {
                                     Thread.sleep(2000);
@@ -103,23 +204,52 @@ public class GetAll extends AppCompatActivity {
                 });
             }
         });
-       /* for(int i = ids.length-1; i>=0; i--) {
-            VKRequest request = new VKRequest("messages.send", VKParameters.from(VKApiConst.USER_ID,
-                    ids[i], VKApiConst.MESSAGE, "messUp"));
-            request.executeWithListener(new VKRequest.VKRequestListener() {
-                @Override
-                public void onComplete(VKResponse response) {
-                    super.onComplete(response);
-                    Log.d("ANSWER", "УСПЕХ");
-                }
-            });
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }*/
+    }
+    private void loadRewardedVideoAd() {
+        mAd.loadAd("ca-app-pub-8154277548860310/7660032948", new AdRequest.Builder()
+                .addTestDevice("4CC1636AE73A6B5D941CFBC28769E6E9")
+                .build());
+        mAd.setRewardedVideoAdListener(this);
     }
 
 
+    @Override
+    public void onRewardedVideoAdLoaded() {
+        mAd.show();
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+
+    }
+
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+
+    }
+
+    int getMyId() {
+        final VKAccessToken vkAccessToken = VKAccessToken.currentToken();
+        return vkAccessToken != null ? Integer.parseInt(vkAccessToken.userId) : 0;
+    }
 }
